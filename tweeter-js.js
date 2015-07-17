0c80e3c048c630ae10db528d246a5a7d623a644a
naomiajacobs <naomiajacobs@gmail.com>
2015-07-17 11:07:22 -0400

    Beginning styling, added show new tweets button

--- tweeter-js.js
+++ tweeter-js.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
-      var $body = $('body');
-      $body.html('');
+  var $body = $('body');
 
-      var index = streams.home.length - 1;
-      while(index >= 0){
-        var tweet = streams.home[index];
-        var $tweet = $('<div></div>');
-        $tweet.text('@' + tweet.user + ': ' + tweet.message);
-        $tweet.appendTo($body);
-        index -= 1;
-      }
+  var showNewTweets = function() {
 
-    });
+    var index = streams.home.length - 1;
+
+    while(index >= 0) {
+      var tweet = streams.home[index];
+      var $tweet = $('<div></div>');
+      var tweetContent = $('<p class="content"></p>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message + '' + tweet.created_at);
+      $tweet.appendTo($('.stream'));
+      index -= 1;
+    }
+  };
+
+  $('.new-tweets').click(showNewTweets);
+});
+ 
