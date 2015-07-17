208ab99e52bda8a74a7c16970319c6b4f33f6817
naomiajacobs <naomiajacobs@gmail.com>
2015-07-17 12:44:16 -0400

    Made tweets appear in reverse order

--- tweeter-js.js
+++ tweeter-js.js
@@ -3,13 +3,16 @@ $(document).ready(function(){
 
   var showNewTweets = function() {
 
+    $('.stream').html('');
+
     var index = streams.home.length - 1;
 
     while(index >= 0) {
       var tweet = streams.home[index];
       var $tweet = $('<div></div>');
-      var tweetContent = $('<p class="content"></p>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + '' + tweet.created_at);
+      var tweetContent = '<p class="content"><a class="username">@' + tweet.user + '</a>: ' + tweet.message + '</p>';
+      var tweetTime = '<p class="time">' + tweet.created_at + '</p>';
+      $tweet.html(tweetContent + tweetTime);
       $tweet.appendTo($('.stream'));
       index -= 1;
     }
