aceae1bb422ad32e2b59f77184ff38e2fd7355e9
naomiajacobs <naomiajacobs@gmail.com>
2015-07-18 12:28:38 -0400

    Made user's stream appear when user is clicked

--- tweeter-js.js
+++ tweeter-js.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  var $body = $('body');
 
   var showNewTweets = function() {
 
@@ -9,8 +8,8 @@ $(document).ready(function(){
 
     while(index >= 0) {
       var tweet = streams.home[index];
-      var $tweet = $('<div></div>');
-      var tweetContent = '<p class="content"><a class="username">@' + tweet.user + '</a>: ' + tweet.message + '</p>';
+      var $tweet = $('<div class="tweet"></div>');
+      var tweetContent = '<p class="content">@<a class="username">' + tweet.user + '</a>: ' + tweet.message + '</p>';
       var tweetTime = '<p class="time">' + tweet.created_at + '</p>';
       $tweet.html(tweetContent + tweetTime);
       $tweet.appendTo($('.stream'));
@@ -18,6 +17,37 @@ $(document).ready(function(){
     }
   };
 
+  showNewTweets();
+
   $('.new-tweets').click(showNewTweets);
+
+  var showUserTweets = function(user) {
+
+    $('.user-tweets').html('');
+
+    var index = streams.users[user].length - 1;
+
+    while(index >= 0) {
+      var tweet = streams.users[user][index];
+      var $tweet = $('<div class="tweet"></div>');
+      var tweetContent = '<p class="content">@<a class="username">' + user + '</a>: ' + tweet.message + '</p>';
+      var tweetTime = '<p class="time">' + tweet.created_at + '</p>';
+      $tweet.html(tweetContent + tweetTime);
+      $tweet.appendTo($('.user-tweets'));
+      index -= 1;
+    }
+
+  };
+
+  $('.tweet').on('click', function() {
+
+    var username = $(this).find('.username').text();
+
+    alert(username);
+
+    showUserTweets(username);
+
+  });
+
 });
  
